b4f37b41d23897c2e4193de1daa57583e36f3ce0
Roma <devilgar@gmail.com>
2020-09-15 22:54:17 +0300

    bigint-opt added

--- README.md
+++ README.md
@@ -1,43 +1,30 @@
 # bigint-optimized
 
-Big Integer implementation, homework from C++ Course in ITMO University, Applied Mathematics and Informatics specialization, repository with other homeworks : [cpp-repository](https://github.com/garipovroma/first-term)
+Big Integer implementation with optimizations, homework from C++ Course in ITMO University, Applied Mathematics and Informatics specialization, repository with other homeworks : [cpp-repository](https://github.com/garipovroma/first-term)
+
+---
 
 `big_integer` implementation includes :
 
 - Default constructor that initializes number to zero `big_integer();`
-
 - Copy constructor, after which the original object and the copy can be modified independently `big_integer(big_integer const& other);`
-
 - Constructor from int `big_integer(int a);`
-
 - Explicit constructor from std :: string `explicit big_integer(std::string const& str);`
-
 - Assignment operator `big_integer& operator=(big_integer const& other);`
-
 - Comparison operators (`==, !=, <, >, <=, >=`)
-
 - Arithmetic operations: addition, subtraction, sign change (unary minus), unary plus
-
 - Multiplication running in time O (nm), where n, m are the lengths of the factors in bits
-
-- Division and remainder of division working in time O (nm), where n is the length of the divisor 
-
-  in bits, and m is the length of the quotient in bits
-
-  Algorithm :  [Multiple-Length Division Revisited: A Tour of the Minefield](http://surface.syr.edu/cgi/viewcontent.cgi?article=1162&context=eecs_techreports)
-
+- Division and remainder of division working in time O (nm), where n is the length of the divisor in bits, and m is the length of the quotient in bits. Algorithm :  [Multiple-Length Division Revisited: A Tour of the Minefield](http://surface.syr.edu/cgi/viewcontent.cgi?article=1162&context=eecs_techreports)
 - Prefix / postfix increment / decrement (`++x, x++, x--`)
-
 - Bitwise operations: and, or, exclusive or, not (`&, |, ^, ~`)
-
 - Bit shifts (`x >> y, x << y, x >>= y, x <<= y`)
-
 - `std::string to_string(big_integer const&)` function
-
 - Big Integer digits recorded in special storage(link) in `uint32_t` type and all bits of storage are used for big int representation
 
-##### Optimizations :
+**Optimizations :**
 
 - Small Object Optimization(Small String Optimization(SSO)): [CppCon 2016: Nicholas Ormrod “The strange details of std::string at Facebook"](https://blogs.msmvps.com/gdicanio/2016/11/17/the-small-string-optimization/), [short article about SSO](https://blogs.msmvps.com/gdicanio/2016/11/17/the-small-string-optimization/)
+
 - Copy on Write Optimization(CoW optimization) : [wikipedia-page](https://en.wikipedia.org/wiki/Copy-on-write) 
 
+  
---
+++ big_integer.h
@@ -0,0 +1,97 @@
+#ifndef BIG_INTEGER_H
+#define BIG_INTEGER_H
+
+#include <algorithm>
+#include "storage.h"
+#include <cstddef>
+#include <functional>
+#include <gmp.h>
+#include <iosfwd>
+#include <string>
+#include <stdexcept>
+
+
+struct big_integer
+{
+    big_integer();
+    big_integer(big_integer const& other);
+    big_integer(int a);
+    explicit big_integer(std::string const& str);
+    ~big_integer();
+    big_integer& operator=(big_integer const& other);
+
+    big_integer& operator+=(big_integer const& rhs);
+    big_integer& operator-=(big_integer const& rhs);
+    big_integer& operator*=(big_integer const& rhs);
+    big_integer& operator/=(big_integer const& rhs);
+    big_integer& operator%=(big_integer const& rhs);
+
+    big_integer& operator&=(big_integer const& rhs);
+    big_integer& operator|=(big_integer const& rhs);
+    big_integer& operator^=(big_integer const& rhs);
+
+    big_integer& operator<<=(int rhs);
+    big_integer& operator>>=(int rhs);
+
+    big_integer operator+() const;
+    big_integer operator-() const;
+    big_integer operator~() const;
+
+    big_integer& operator++();
+    big_integer operator++(int);
+
+    big_integer& operator--();
+    big_integer operator--(int);
+
+    friend bool operator==(big_integer const& a, big_integer const& b);
+    friend bool operator!=(big_integer const& a, big_integer const& b);
+    friend bool operator<(big_integer const& a, big_integer const& b);
+    friend bool operator>(big_integer const& a, big_integer const& b);
+    friend bool operator<=(big_integer const& a, big_integer const& b);
+    friend bool operator>=(big_integer const& a, big_integer const& b);
+
+    friend std::string to_string(big_integer const& a);
+
+    friend std::string to_string(big_integer const& a);
+    friend void swap(big_integer &a, big_integer &b);
+private:
+    static storage multiply(const std::vector<uint32_t> &a, const std::vector<uint32_t> &b);
+    uint32_t operator[](size_t pos) const;
+    big_integer& shrink_to_fit();
+    void fill(size_t size);
+    big_integer& div(uint32_t b);
+    big_integer& mul(uint32_t rhs);
+    big_integer& negate();
+    big_integer abs() const;
+    big_integer& bit_operator(big_integer const& a,  const std::function<uint32_t(uint32_t, uint32_t)> &function);
+    uint32_t get_end_of_mas() const;
+private:
+    storage mas;
+    bool sign;
+};
+
+
+big_integer operator+(big_integer a, big_integer const& b);
+big_integer operator-(big_integer a, big_integer const& b);
+big_integer operator*(big_integer a, big_integer const& b);
+big_integer operator/(big_integer a, big_integer const& b);
+big_integer operator%(big_integer a, big_integer const& b);
+
+big_integer operator&(big_integer a, big_integer const& b);
+big_integer operator|(big_integer a, big_integer const& b);
+big_integer operator^(big_integer a, big_integer const& b);
+
+big_integer operator<<(big_integer a, int b);
+big_integer operator>>(big_integer a, int b);
+
+bool operator==(big_integer const& a, big_integer const& b);
+bool operator!=(big_integer const& a, big_integer const& b);
+bool operator<(big_integer const& a, big_integer const& b);
+bool operator>(big_integer const& a, big_integer const& b);
+bool operator<=(big_integer const& a, big_integer const& b);
+bool operator>=(big_integer const& a, big_integer const& b);
+
+std::string to_string(big_integer const& a);
+std::ostream& operator<<(std::ostream& s, big_integer const& a);
+
+#endif // BIG_INTEGER_H
